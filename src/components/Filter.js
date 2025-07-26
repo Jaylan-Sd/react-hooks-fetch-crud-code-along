010be9067c2ffca8692f66bb7a92c70282d72be8
Jaylan <jaylansaad20@gmail.com>
2025-07-26 21:41:22 +0300

    completed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,12 +3,8 @@ import React from "react";
 function Filter({ category, onCategoryChange }) {
   return (
     <div className="Filter">
-      <select
-        name="filter"
-        value={category}
-        onChange={(e) => onCategoryChange(e.target.value)}
-      >
-        <option value="All">Filter by category</option>
+      <select value={category} onChange={(e) => onCategoryChange(e.target.value)}>
+        <option value="All">All</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
         <option value="Dessert">Dessert</option>
